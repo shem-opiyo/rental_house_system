de19b72db619936af7016cdd067b0504f1959c2a
shem-opiyo <shemnicholasopiyo@gmail.com>
2024-05-12 10:29:05 +0300

    initial commit of the house rent management system sample

---
+++ src/main/java/House_rent_management_system.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author shonic
+ */
+public class House_rent_management_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
